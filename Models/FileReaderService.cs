195b8c966e5e5351c661031dff181902c77e8822
silviu <183800984+silviu28@users.noreply.github.com>
2025-06-01 21:02:28 +0300

    Add NAudio dependency for audio manip

--- Models/FileReaderService.cs
+++ Models/FileReaderService.cs
@@ -1,5 +1,6 @@
 using System.Collections.ObjectModel;
 using System.IO;
+using NAudio;
 
 namespace MusicPlayer.Models;
 
--- ViewModels/PlayerViewModel.cs
+++ ViewModels/PlayerViewModel.cs
@@ -4,18 +4,19 @@ using System.Windows.Input;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using MusicPlayer.Models;
+using NAudio.Wave;
 
 namespace MusicPlayer.ViewModels;
 
 public class PlayerViewModel : ViewModelBase
 {
     public ObservableCollection<string> MusicListings { get; set; } = FileReaderService.GetAll();
-    
+
     private int _currentPtr = 0;
     public string Current => MusicListings[_currentPtr];
 
     public bool IsPlaying { get; set; }
-    
+
     public double SongProgress { get; set; }
 
     private double _volumeValue;
@@ -25,29 +26,100 @@ public class PlayerViewModel : ViewModelBase
         get => _volumeValue;
         set => SetProperty(ref _volumeValue, Math.Round(value));
     }
-    
+
+    private void OnButtonPlay(object? sender, EventArgs e)
+    {
+        if (_audioOutput is null)
+        {
+            _audioOutput = new();
+            _audioOutput.PlaybackStopped += OnPlaybackStop;
+        }
+
+        if (_audioReader is null)
+        {
+            _audioReader = new(@".");
+            _audioOutput.Init(_audioReader);
+        }
+
+        _audioOutput.Play();
+    }
+
+    private void OnPlaybackStop(object? sender, EventArgs e)
+    {
+        _audioOutput?.Dispose();
+        _audioOutput = null;
+        _audioReader?.Dispose();
+        _audioReader = null;
+    }
+
+
+    private WaveOutEvent? _audioOutput;
+
+    public WaveOutEvent AudioOutput
+    {
+        get
+        {
+            if (_audioOutput is null)
+            {
+                _audioOutput = new WaveOutEvent();
+                _audioOutput.PlaybackStopped += (_, _) =>
+                {
+                    AudioOutput.Dispose();
+                    _audioOutput = null;
+                    AudioReader.Dispose();
+                    _audioReader = null;
+                };
+            }
+
+            return _audioOutput;
+        }
+    }
+
+    private AudioFileReader? _audioReader;
+    public AudioFileReader AudioReader
+    {
+        get
+        {
+            if (_audioReader is null)
+            {
+                _audioReader = new(Current);
+                AudioOutput.Init(_audioReader);
+            }
+            return _audioReader;
+        }
+    }
+
     #region Comms
+
     private ICommand? _togglePlayCommand;
     public ICommand TogglePlayCommand => _togglePlayCommand ??= new RelayCommand(TogglePlay);
+
     private void TogglePlay()
     {
-        this.IsPlaying = !this.IsPlaying;
+        IsPlaying = !IsPlaying;
+        // if (!IsPlaying)
+        //     AudioOutput.Stop();
+        // else
+        //     AudioOutput.Play();
         OnPropertyChanged(nameof(this.IsPlaying));
     }
 
     private ICommand? _forwardCommand;
+
     public ICommand ForwardCommand => _forwardCommand ??= new RelayCommand(() =>
     {
         if (_currentPtr < MusicListings.Count - 1) ++_currentPtr;
         OnPropertyChanged(nameof(Current));
     });
-    
+
     private ICommand? _reverseCommand;
+
     public ICommand ReverseCommand => _reverseCommand ??= new RelayCommand(() =>
     {
         if (_currentPtr > 0) --_currentPtr;
         OnPropertyChanged(nameof(Current));
     });
+
     #endregion
 
     public PlayerViewModel()
